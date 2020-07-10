f5804a83399fd49a30c2b43c338aa3e086752ffb
RegiBrazil <rbarosa@us.ibm.com>
2020-07-10 18:02:28 -0400

    * chnaged July 10 - 18:02

--- samples/MortgageApplication/cobol/epsnbrvl.cbl
+++ samples/MortgageApplication/cobol/epsnbrvl.cbl
@@ -1,6 +1,6 @@
        ID DIVISION.
        PROGRAM-ID. EPSNBRVL
-      * chnaged July 10 - 18:00
+      * chnaged July 10 - 18:02
       *    THIS IS A CALLED PROGRAM EXAMPLE FOR DEMONSTRATION
       *
       *    THIS PROGRAM WILL BE CALLED BY ANOTHER, RECEIVE
