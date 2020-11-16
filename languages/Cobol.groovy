b7d794634ad4a6b92bc13b36e5bc1658e5e31940
MATHIEUDalbin <MATHIEUDalbin@DESKTOP-KEJLP1T.mshome.net>
2020-11-16 18:00:23 +0100

    bbbbb

--- languages/Cobol.groovy
+++ languages/Cobol.groovy
@@ -208,7 +208,7 @@ def createCompileCommand(String buildFile, LogicalFile logicalFile, String membe
 		compile.dd(new DDStatement().dsn(props.SCSQCOBC).options("shr"))
 
 	// add concatenation
-	println("Cobol compiler= " + props.cobol_compiler + "    - Concat= " + props.cobol_SYSLIBConcat)
+	println("Cobol compiler= " + props.cobol_compiler + "    - Concat= " + props.cobol_SYSLIBConcatenation)
 	def SYSLIBConcatenation = props.getFileProperty('cobol_SYSLIBConcatenation', buildFile) ?: props.cobol_SYSLIBConcat
 	if (SYSLIBConcatenation) {
 		def String[] SYSLIBDatasets = SYSLIBConcatenation.split(',');
