159b24b16b37e21be45c2ccd6ab70ed1dedbdcde
Dennis Behm <dennis.behm@de.ibm.com>
2020-11-26 12:48:50 +0100

    display zunit result

--- languages/ZunitConfig.groovy
+++ languages/ZunitConfig.groovy
@@ -131,13 +131,15 @@ buildUtils.createLanguageDatasets(langQualifier)
 		// manage processing the RC, up to your logic. You might want to flag the build as failed.
 		if (rc <= props.zunit_maxPassRC.toInteger()){
 			println   "***  zUnit Test Job ${zUnitRunJCL.submittedJobId} completed with $rc "
-			printReport(reportLogFile)
 			// Store Report in Workspace
 			new CopyToHFS().dataset(props.zunit_bzureportPDS).member(member).file(reportLogFile).hfsEncoding(props.logEncoding).append(false).copy()
+			// printReport 
+			printReport(reportLogFile)
 		} else if (rc <= props.zunit_maxWarnRC.toInteger()){
 			String warningMsg = "*! The zunit test returned a warning ($rc) for $buildFile"
 			// Store Report in Workspace
 			new CopyToHFS().dataset(props.zunit_bzureportPDS).member(member).file(reportLogFile).hfsEncoding(props.logEncoding).append(false).copy()
+			// print warning and report
 			println warningMsg
 			printReport(reportLogFile)
 			buildUtils.updateBuildResult(warningMsg:warningMsg,logs:["${member}_zunit.log":logFile],client:getRepositoryClient())
