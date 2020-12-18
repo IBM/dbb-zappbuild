c3b31ea72a1ffe39ae7a1bdae45fbf9e3e1a0570
Dennis Behm <dennis.behm@de.ibm.com>
2020-12-18 09:36:37 +0100

    Add SDSNLOAD to SYSLIB for LinkEdit

--- languages/PLI.groovy
+++ languages/PLI.groovy
@@ -246,6 +246,9 @@ def createLinkEditCommand(String buildFile, LogicalFile logicalFile, String memb
 	if (buildUtils.isCICS(logicalFile))
 		linkedit.dd(new DDStatement().dsn(props.SDFHLOAD).options("shr"))
 
+	if (buildUtils.isSQL(logicalFile))
+		linkedit.dd(new DDStatement().dsn(props.SDSNLOAD).options("shr"))
+
 	// add a copy command to the linkedit command to append the SYSPRINT from the temporary dataset to the HFS log file
 	linkedit.copy(new CopyToHFS().ddName("SYSPRINT").file(logFile).hfsEncoding(props.logEncoding).append(true))
 
