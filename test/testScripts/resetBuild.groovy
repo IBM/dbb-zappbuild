1a8c5c090c1dd86c980b179fded5bba0c043387d
ArthiIreddy <46818025+ArthiIreddy@users.noreply.github.com>
2021-03-26 10:05:39 -0400

    Update resetBuild.groovy

--- test/testScripts/resetBuild.groovy
+++ test/testScripts/resetBuild.groovy
@@ -24,6 +24,7 @@ resetBuildCommand << "--url ${props.url}"
 resetBuildCommand << "--id ${props.id}"
 resetBuildCommand << (props.pw ? "--pw ${props.pw}" : "--pwFile ${props.pwFile}")
 resetBuildCommand << (props.verbose ? "--verbose" : "")
+resetBuildCommand << (props.propFile ? "--f ${props.f}" : "--propFile ${props.propFile}")
 resetBuildCommand << "--reset"
 
 // Run reset build 
