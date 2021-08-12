9b365a1be147ea4881697dd3b151c0789c0c1af9
Dennis Behm <dennis.behm@de.ibm.com>
2021-08-12 13:07:04 +0200

    pass the correct langPrefix for assembler (#134)
    * pass the correct langPrefix for assembler
    
    * Updated descriptions for Linkcards

--- languages/Assembler.groovy
+++ languages/Assembler.groovy
@@ -305,7 +305,7 @@ def createLinkEditCommand(String buildFile, LogicalFile logicalFile, String memb
 
 	// add DD statements to the linkedit command
 	String assembler_loadPDS = props.getFileProperty('assembler_loadPDS', buildFile)
-	String deployType = buildUtils.getDeployType("linkedit", buildFile, logicalFile)
+	String deployType = buildUtils.getDeployType("assembler", buildFile, logicalFile)
 	linkedit.dd(new DDStatement().name("SYSLMOD").dsn("${assembler_loadPDS}($member)").options('shr').output(true).deployType(deployType))
 	linkedit.dd(new DDStatement().name("SYSPRINT").options(props.assembler_tempOptions))
 	linkedit.dd(new DDStatement().name("SYSUT1").options(props.assembler_tempOptions))
