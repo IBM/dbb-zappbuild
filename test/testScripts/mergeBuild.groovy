7d51f2d3f863992a33b10cd5df7c22dc2d3c7449
Dennis Behm <dennis.behm@de.ibm.com>
2021-11-16 17:37:30 +0100

    adding test case for mergeBuild

--- test/testScripts/mergeBuild.groovy
+++ test/testScripts/mergeBuild.groovy
@@ -12,51 +12,51 @@ println "\n** Executing test script mergeBuild.groovy"
 def dbbHome = EnvVars.getHome()
 if (props.verbose) println "** DBB_HOME = ${dbbHome}"
 
-// create impact build command
-def impactBuildCommand = []
-impactBuildCommand << "${dbbHome}/bin/groovyz"
-impactBuildCommand << "${props.zAppBuildDir}/build.groovy"
-impactBuildCommand << "--workspace ${props.workspace}"
-impactBuildCommand << "--application ${props.app}"
-impactBuildCommand << (props.outDir ? "--outDir ${props.outDir}" : "--outDir ${props.zAppBuildDir}/out")
-impactBuildCommand << "--hlq ${props.hlq}"
-impactBuildCommand << "--logEncoding UTF-8"
-impactBuildCommand << "--url ${props.url}"
-impactBuildCommand << "--id ${props.id}"
-impactBuildCommand << (props.pw ? "--pw ${props.pw}" : "--pwFile ${props.pwFile}")
-impactBuildCommand << (props.verbose ? "--verbose" : "")
-impactBuildCommand << (props.propFiles ? "--propFiles ${props.propFiles}" : "")
-impactBuildCommand << "--mergeBuild"
+// create merge build command
+def mergeBuildCommand = []
+mergeBuildCommand << "${dbbHome}/bin/groovyz"
+mergeBuildCommand << "${props.zAppBuildDir}/build.groovy"
+mergeBuildCommand << "--workspace ${props.workspace}"
+mergeBuildCommand << "--application ${props.app}"
+mergeBuildCommand << (props.outDir ? "--outDir ${props.outDir}" : "--outDir ${props.zAppBuildDir}/out")
+mergeBuildCommand << "--hlq ${props.hlq}"
+mergeBuildCommand << "--logEncoding UTF-8"
+mergeBuildCommand << "--url ${props.url}"
+mergeBuildCommand << "--id ${props.id}"
+mergeBuildCommand << (props.pw ? "--pw ${props.pw}" : "--pwFile ${props.pwFile}")
+mergeBuildCommand << (props.verbose ? "--verbose" : "")
+mergeBuildCommand << (props.propFiles ? "--propFiles ${props.zAppBuildDir}/test/applications/${props.app}/${props.mergeBuild_buildPropSetting},${props.propFiles}" : "")
+mergeBuildCommand << "--mergeBuild"
 
-// iterate through change files to test impact build
+// iterate through change files to test merge build
 @Field def assertionList = []
 PropertyMappings filesBuiltMappings = new PropertyMappings('mergeBuild_expectedFilesBuilt')
 def changedFiles = props.mergeBuild_changedFiles.split(',')
 println("** Processing changed files from mergeBuild_changedFiles property : ${props.mergeBuild_changedFiles}")
 try {
 	changedFiles.each { changedFile ->
-		println "\n** Running impact build test for changed file $changedFile"
+		println "\n** Running merge build test for changed file $changedFile"
 		
 		// update changed file in Git repo test branch
 		copyAndCommit(changedFile)
 		
-		// run impact build
-		println "** Executing ${impactBuildCommand.join(" ")}"
+		// run merge build
+		println "** Executing ${mergeBuildCommand.join(" ")}"
 		def outputStream = new StringBuffer()
-		def process = ['bash', '-c', impactBuildCommand.join(" ")].execute()
+		def process = ['bash', '-c', mergeBuildCommand.join(" ")].execute()
 		process.waitForProcessOutput(outputStream, System.err)
 		
 		// validate build results
-		validateImpactBuild(changedFile, filesBuiltMappings, outputStream)
+		validateMergeBuild(changedFile, filesBuiltMappings, outputStream)
 	}
 }
 finally {
 	cleanUpDatasets()
 	if (assertionList.size()>0) {
         println "\n***"
-	println "**START OF FAILED IMPACT BUILD TEST RESULTS**\n"
-	println "*FAILED IMPACT BUILD TEST RESULTS*\n" + assertionList
-	println "\n**END OF FAILED IMPACT BUILD TEST RESULTS**"
+	println "**START OF FAILED MERGED BUILD TEST RESULTS**\n"
+	println "*FAILED MERGED BUILD TEST RESULTS*\n" + assertionList
+	println "\n**END OF FAILED MERGED BUILD TEST RESULTS**"
 	println "***"
   }
 }
@@ -79,24 +79,24 @@ def copyAndCommit(String changedFile) {
 	task.waitForProcessOutput(outputStream, System.err)
 }
 
-def validateImpactBuild(String changedFile, PropertyMappings filesBuiltMappings, StringBuffer outputStream) {
+def validateMergeBuild(String changedFile, PropertyMappings filesBuiltMappings, StringBuffer outputStream) {
 
-	println "** Validating impact build results"
+	println "** Validating merge build results"
 	def expectedFilesBuiltList = filesBuiltMappings.getValue(changedFile).split(',')
 	
     try{
 	// Validate clean build
-	assert outputStream.contains("Build State : CLEAN") : "*! IMPACT BUILD FAILED FOR $changedFile\nOUTPUT STREAM:\n$outputStream\n"
+	assert outputStream.contains("Build State : CLEAN") : "*! MERGED BUILD FAILED FOR $changedFile\nOUTPUT STREAM:\n$outputStream\n"
 
 	// Validate expected number of files built
-	def numImpactFiles = expectedFilesBuiltList.size()
-	assert outputStream.contains("Total files processed : ${numImpactFiles}") : "*! IMPACT BUILD FOR $changedFile TOTAL FILES PROCESSED ARE NOT EQUAL TO ${numImpactFiles}\nOUTPUT STREAM:\n$outputStream\n"
+	def numMergeFiles = expectedFilesBuiltList.size()
+	assert outputStream.contains("Total files processed : ${numMergeFiles}") : "*! MERGED BUILD FOR $changedFile TOTAL FILES PROCESSED ARE NOT EQUAL TO ${numMergeFiles}\nOUTPUT STREAM:\n$outputStream\n"
 
 	// Validate expected built files in output stream
-	assert expectedFilesBuiltList.count{ i-> outputStream.contains(i) } == expectedFilesBuiltList.size() : "*! IMPACT BUILD FOR $changedFile DOES NOT CONTAIN THE LIST OF BUILT FILES EXPECTED ${expectedFilesBuiltList}\nOUTPUT STREAM:\n$outputStream\n"
+	assert expectedFilesBuiltList.count{ i-> outputStream.contains(i) } == expectedFilesBuiltList.size() : "*! MERGED BUILD FOR $changedFile DOES NOT CONTAIN THE LIST OF BUILT FILES EXPECTED ${expectedFilesBuiltList}\nOUTPUT STREAM:\n$outputStream\n"
 	
 	println "**"
-	println "** IMPACT BUILD TEST : PASSED FOR $changedFile **"
+	println "** MERGED BUILD TEST : PASSED FOR $changedFile **"
 	println "**"
     }
     catch(AssertionError e) {
@@ -107,7 +107,7 @@ def validateImpactBuild(String changedFile, PropertyMappings filesBuiltMappings,
 def cleanUpDatasets() {
 	def segments = props.mergeBuild_datasetsToCleanUp.split(',')
 	
-	println "Deleting impact build PDSEs ${segments}"
+	println "Deleting merge build PDSEs ${segments}"
 	segments.each { segment ->
 	    def pds = "'${props.hlq}.${segment}'"
 	    if (ZFile.dsExists(pds)) {
