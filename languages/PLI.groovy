3d52cad13bbec6d85a2edcd3740bca49adb16114
Dennis Behm <dennis.behm@de.ibm.com>
2022-02-11 15:20:24 +0100

    Correction about supported build types for SSI info (#174)
    * Correction to be specific about supported build types for SSI info

--- languages/PLI.groovy
+++ languages/PLI.groovy
@@ -256,7 +256,7 @@ def createLinkEditCommand(String buildFile, LogicalFile logicalFile, String memb
 
 	// obtain githash for buildfile
 	String pli_storeSSI = props.getFileProperty('pli_storeSSI', buildFile)
-	if (pli_storeSSI && pli_storeSSI.toBoolean()) {
+	if (pli_storeSSI && pli_storeSSI.toBoolean() && (props.mergeBuild || props.impactBuild)) {
 		String ssi = buildUtils.getShortGitHash(buildFile)
 		if (ssi != null) parms = parms + ",SSI=$ssi"
 	}
