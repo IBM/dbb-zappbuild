78129a55100f17421d53d806fd4fdd1fc449faa8
Dennis Behm <dennis.behm@de.ibm.com>
2022-03-30 17:47:05 +0200

    syntax check

--- utilities/BuildUtilities.groovy
+++ utilities/BuildUtilities.groovy
@@ -17,8 +17,8 @@ import groovy.ant.*
 @Field def gitUtils = loadScript(new File("GitUtilities.groovy"))
 
 // Conditionally load the ResolverUtilities.groovy which require at least DBB 1.1.2
-if (props.useSearchConfiguration && props.useSearchConfiguration.toBoolean() && buildUtils.assertDbbBuildToolkitVersion(props.dbbToolkitVersion, "1.1.2"))
-	@Field def resolverUtils = loadScript(new File("ResolverUtilities.groovy"))
+if (props.useSearchConfiguration && props.useSearchConfiguration.toBoolean() && buildUtils.assertDbbBuildToolkitVersion(props.dbbToolkitVersion, "1.1.2")) {
+	@Field def resolverUtils = loadScript(new File("ResolverUtilities.groovy")) }
 
 
 /*
--- utilities/ImpactUtilities.groovy
+++ utilities/ImpactUtilities.groovy
@@ -16,8 +16,8 @@ import java.util.regex.*
 @Field String hashPrefix = ':githash:'
 
 // Conditionally load the ResolverUtilities.groovy which require at least DBB 1.1.2
-if (props.useSearchConfiguration && props.useSearchConfiguration.toBoolean() && buildUtils.assertDbbBuildToolkitVersion(props.dbbToolkitVersion, "1.1.2"))
-	@Field def resolverUtils = loadScript(new File("ResolverUtilities.groovy"))
+if (props.useSearchConfiguration && props.useSearchConfiguration.toBoolean() && buildUtils.assertDbbBuildToolkitVersion(props.dbbToolkitVersion, "1.1.2")) {
+	@Field def resolverUtils = loadScript(new File("ResolverUtilities.groovy")) }
 
 
 def createImpactBuildList(RepositoryClient repositoryClient) {
