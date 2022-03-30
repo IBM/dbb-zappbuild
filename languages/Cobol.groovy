6c7094ff4d064cc2f02b356b7b6aff6de11090a0
Dennis Behm <dennis.behm@de.ibm.com>
2022-03-30 18:01:10 +0200

    fix syntax error

--- languages/Cobol.groovy
+++ languages/Cobol.groovy
@@ -16,8 +16,8 @@ import com.ibm.dbb.build.report.records.*
 @Field RepositoryClient repositoryClient
 
 // Conditionally load the ResolverUtilities.groovy which require at least DBB 1.1.2
-if (props.useSearchConfiguration && props.useSearchConfiguration.toBoolean() && buildUtils.assertDbbBuildToolkitVersion(props.dbbToolkitVersion, "1.1.2"))
-	@Field def resolverUtils = loadScript(new File("${props.zAppBuildDir}/utilities/ResolverUtilities.groovy"))
+if (props.useSearchConfiguration && props.useSearchConfiguration.toBoolean() && buildUtils.assertDbbBuildToolkitVersion(props.dbbToolkitVersion, "1.1.2")) {
+	@Field def resolverUtils = loadScript(new File("${props.zAppBuildDir}/utilities/ResolverUtilities.groovy"))}
 	
 println("** Building files mapped to ${this.class.getName()}.groovy script")
 
