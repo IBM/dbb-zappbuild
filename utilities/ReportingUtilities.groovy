4974e5aab242dde05fb1d83adf1d44c6e9b72f4c
Dennis Behm <dennis.behm@de.ibm.com>
2022-08-04 13:33:44 +0200

    Revised implementation of reporting of external impacts
    Signed-off-by: Dennis Behm <dennis.behm@de.ibm.com>

--- utilities/ReportingUtilities.groovy
+++ utilities/ReportingUtilities.groovy
@@ -234,3 +234,47 @@ def generateConcurrentChangesReports(Set<String> buildList, Set<String> concurre
 		}
 	}
 }
+
+def matches(String file, List<PathMatcher> pathMatchers) {
+	def result = pathMatchers.any { matcher ->
+		Path path = FileSystems.getDefault().getPath(file);
+		if ( matcher.matches(path) )
+		{
+			return true
+		}
+	}
+	return result
+}
+
+/**
+ * createPathMatcherPattern
+ * Generic method to build PathMatcher from a build property
+ */
+
+def createPathMatcherPattern(String property) {
+	List<PathMatcher> pathMatchers = new ArrayList<PathMatcher>()
+	if (property) {
+		property.split(',').each{ filePattern ->
+			if (!filePattern.startsWith('glob:') || !filePattern.startsWith('regex:'))
+				filePattern = "glob:$filePattern"
+			PathMatcher matcher = FileSystems.getDefault().getPathMatcher(filePattern)
+			pathMatchers.add(matcher)
+		}
+	}
+	return pathMatchers
+}
+
+/**
+ * create List of Regex Patterns
+ */
+
+def createMatcherPatterns(String property) {
+	List<Pattern> patterns = new ArrayList<Pattern>()
+	if (property) {
+		property.split(',').each{ patternString ->
+			Pattern pattern = Pattern.compile(patternString);
+			patterns.add(pattern)
+		}
+	}
+	return patterns
+}
