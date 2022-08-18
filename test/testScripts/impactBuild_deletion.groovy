7cda42de961b117f854e09d34e1859f685252378
Dennis Behm <dennis.behm@de.ibm.com>
2022-08-18 10:29:26 +0200

    setting a baseline built
    Signed-off-by: Dennis Behm <dennis.behm@de.ibm.com>

--- test/testScripts/impactBuild_deletion.groovy
+++ test/testScripts/impactBuild_deletion.groovy
@@ -12,6 +12,22 @@ println "\n** Executing test script impactBuild_deletion.groovy"
 def dbbHome = EnvVars.getHome()
 if (props.verbose) println "** DBB_HOME = ${dbbHome}"
 
+// Create full build command to set baseline and populate output libraries
+def fullBuildCommand = []
+fullBuildCommand << "${dbbHome}/bin/groovyz"
+fullBuildCommand << "${props.zAppBuildDir}/build.groovy"
+fullBuildCommand << "--workspace ${props.workspace}"
+fullBuildCommand << "--application ${props.app}"
+fullBuildCommand << (props.outDir ? "--outDir ${props.outDir}" : "--outDir ${props.zAppBuildDir}/out")
+fullBuildCommand << "--hlq ${props.hlq}"
+fullBuildCommand << "--logEncoding UTF-8"
+fullBuildCommand << "--url ${props.url}"
+fullBuildCommand << "--id ${props.id}"
+fullBuildCommand << (props.pw ? "--pw ${props.pw}" : "--pwFile ${props.pwFile}")
+fullBuildCommand << (props.verbose ? "--verbose" : "")
+fullBuildCommand << (props.propFiles ? "--propFiles ${props.propFiles},${props.zAppBuildDir}/test/applications/${props.app}/${props.impactBuild_deletion_buildPropSetting}" : "")
+fullBuildCommand << "--fullBuild"
+
 // create impact build command
 def impactBuildCommand = []
 impactBuildCommand << "${dbbHome}/bin/groovyz"
@@ -36,6 +52,19 @@ PropertyMappings outputsDeletedMappings = new PropertyMappings('impactBuild_dele
 
 def deleteFiles = props.impactBuild_deletion_deleteFiles.split(',')
 try {
+	
+	println "\n** Running full build to set baseline"
+
+	// run impact build
+	println "** Executing ${fullBuildCommand.join(" ")}"
+	def outputStream = new StringBuffer()
+	def process = [
+		'bash',
+		'-c',
+		fullBuildCommand.join(" ")
+	].execute()
+	process.waitForProcessOutput(outputStream, System.err)
+	
 	deleteFiles.each{ deleteFile ->
 		
 		// delete file in Git repo test branch
