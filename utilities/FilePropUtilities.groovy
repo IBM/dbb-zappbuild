ea25d7f43064911457f93d0587d67ea318d0e033
Anuprakash Moothedath <anuprakash.moothedath@ibm.com>
2023-02-07 10:57:29 +0530

    Reworded comments and added warning in BuildReport.json
    Signed-off-by: Anuprakash Moothedath <anuprakash.moothedath@ibm.com>

--- utilities/FilePropUtilities.groovy
+++ utilities/FilePropUtilities.groovy
@@ -1,6 +1,8 @@
 @groovy.transform.BaseScript com.ibm.dbb.groovy.ScriptLoader baseScript
 import com.ibm.dbb.build.*
 import groovy.transform.*
+import com.ibm.dbb.build.report.records.*
+import com.ibm.dbb.build.report.*
 
 // define script properties
 @Field BuildProperties props = BuildProperties.getInstance()
@@ -57,7 +59,7 @@ def loadFileLevelPropertiesFromFile(List<String> buildList) {
 	             fileLevelProps.load(propertyFileIS)
 
 	             fileLevelProps.entrySet().each { entry ->
-	                 if (props.verbose) println "    Found file property ${entry.key} = ${entry.value} for ${buildFile}"
+	                 if (props.verbose) println "    Found file property ${entry.key} = ${entry.value}"
 	                 filePropMap[entry.key] = entry.value 
 	             }
 	         } else {
@@ -66,7 +68,7 @@ def loadFileLevelPropertiesFromFile(List<String> buildList) {
 	     }
 	        
 	     // Add the file patterns from file property map after checking the existence of the file patterns
-	     if (props.verbose) println "*** Checking for the existing file property overrides"
+	     if (props.verbose) println "*** Checking for existing file property overrides"
 	     filePropMap.each { entry ->
 	         // Check if the file property definition already exists
 	         (filePatternIsMapped, filePatternIsMappedAtFileName, noChangeFilePattern, currValue) = checkExistingFilesPropertyDefinition(buildFile, member, entry.key)
@@ -75,33 +77,37 @@ def loadFileLevelPropertiesFromFile(List<String> buildList) {
 	         if (filePatternIsMapped ) {
 	            if (filePatternIsMappedAtFileName) {
 	                props.removeFileProperty(entry.key)
-	                if (props.verbose) println "       Deleted the file property ${entry.key}"
 	                noChangeFilePattern.each { noChangeFile ->
+	                    if (props.verbose) println "       Retaining ${entry.key} = ${noChangeFile.value} for ${noChangeFile.key}"
 	                    props.addFilePattern(entry.key, noChangeFile.value, noChangeFile.key)
-	                    if (props.verbose) println "       Added from backup ${entry.key} = ${noChangeFile.value} for ${noChangeFile.key}"
 	                 }
 	                 // Add the buildFile file pattern with new value    
+	                 if (props.verbose) println "    Updating ${entry.key} = ${entry.value} for ${buildFile}"
 	                 props.addFilePattern(entry.key, entry.value, buildFile)
-	                 if (props.verbose) println "    Added ${entry.key} = ${entry.value} for ${buildFile}"
 	            } else {
-	                println("    *! Warning: $buildFile is already mapped as a file pattern as part of a wildcard mapping in file.properties.")
-	                println("    *! Warning: Please check the existing file property list below and fix it to fetch the correct file properties.")
-	            	println("    *! File Property list :")
+	                println("    *! Warning: $buildFile is already mapped as a file pattern as part of a file group wildcard possibly in file.properties.")
+	                println("    *! Warning: Please check the existing file property list below and fix it to contain the correct file properties.")
+	            	println("    *! Existing File Property value and patterns :")
 	            	noChangeFilePattern.each { noChangeFile ->
 	            	    println("    *!     ${entry.key} = ${noChangeFile.value} for ${noChangeFile.key}")
 	            	}
-	                println("    *! Warning: Override for $buildFile skipped. Existing file property value used: ${entry.key} = ${currValue}")
+	                println("    *! Warning: Override for $buildFile could not be applied. Existing file property value will be used: ${entry.key} = ${currValue}")
+	                
+	                // Add file property warning messages to BuildReport.json
+	                addFilePropWarningRecord(buildFile, noChangeFilePattern, entry.key, entry.value, currValue)
 	            }
 	         } else {
 	             // Add the buildFile file pattern with new value    
+	             if (props.verbose) println "    Adding ${entry.key} = ${entry.value} for ${buildFile}"
 	             props.addFilePattern(entry.key, entry.value, buildFile)
-	             if (props.verbose) println "    Added ${entry.key} = ${entry.value} for ${buildFile}"
 	         }
 	     }
 	 }
 }
 
-// Add the language configuration properties to the file property map
+/*
+ * Add the language configuration properties to the file property map
+ */ 
 def loadProgramTypeProperties(String languageConfigurationPropertyFileName, String languageConfigurationPropertyFile, String buildFile) {
 	    
     def filePropMap = [:]
@@ -114,17 +120,19 @@ def loadProgramTypeProperties(String languageConfigurationPropertyFileName, Stri
     languageConfigProps.load(languageConfigurationPropertyFileIS)
 	    
     languageConfigProps.entrySet().each { entry ->
-       if (props.verbose) println "    Found language configuration property ${entry.key} = ${entry.value} for $buildFile"
+       if (props.verbose) println "    Found language configuration property ${entry.key} = ${entry.value}"
        filePropMap[entry.key] = entry.value
     }
     return filePropMap
 }
 
-// Check if file is already mapped to the build property 
-// Return the file property map flag and backed-up file patterns
+/*
+ * Check if file is already mapped to the build property
+ * Return the file property map flag and backed-up file patterns
+ */
 def checkExistingFilesPropertyDefinition(String buildFile, String member, String entryKey){
 	    
-    if (props.verbose) println "    Checking the existing file property ==> ${entryKey}"
+    if (props.verbose) println "    Checking build property ${entryKey}"
     PropertyMappings propertyMapping = new PropertyMappings(entryKey)
     def propertyMappingValues = propertyMapping.getValues()
     String expValue = ""
@@ -166,9 +174,41 @@ def checkExistingFilesPropertyDefinition(String buildFile, String member, String
                 filePatternIsMappedAtFileName = true   
             } else { 
                 noChangeFilePattern[filePattern] = value
-                if (props.verbose) println("       Backup ${entryKey} = ${value} for ${filePattern}")
+                if (props.verbose) println("       Retaining ${entryKey} = ${value} for ${filePattern}")
             }
         }
     }   
     return [filePatternIsMapped, filePatternIsMappedAtFileName, noChangeFilePattern, currValue]
+}
+
+/*
+ * Add file property warning messages to BuildReport.json
+ */
+def addFilePropWarningRecord(String buildFile, Map noChangeFilePattern, String entryKey, String entryValue, String currValue){
+	
+	if (props.verbose) println("    *! Inserting warning message in BuildReport.json")
+	
+	// New Generic Property Record
+	def currPropList = []
+	PropertiesRecord filePropWarningInfo = new PropertiesRecord("File Property Warning : ${buildFile}")
+	
+
+	// Add file property override warning info
+	def warningMsg1 = "$buildFile is already mapped as a file pattern as part of a file group wildcard possibly in file.properties."
+	def warningMsg2 = "Please check the existing file property list below and fix it to contain the correct file properties."
+	def warningMsg3 = "Override for $buildFile could not be applied. Existing file property value will be used: ${entryKey} = ${currValue}"
+	def warningMsg4 =  "Existing File Property value and patterns in existingFilePropList:"
+	noChangeFilePattern.each { noChangeFile ->
+	    currPropList.add("${entryKey} = ${noChangeFile.value} for ${noChangeFile.key}")
+	}
+	
+	// Add warning messages to BuildReport.json
+	filePropWarningInfo.addProperty("warningMsg1",warningMsg1)	
+	filePropWarningInfo.addProperty("warningMsg2",warningMsg2)
+	filePropWarningInfo.addProperty("warningMsg3",warningMsg3)
+	filePropWarningInfo.addProperty("warningMsg4",warningMsg4)
+	filePropWarningInfo.addProperty("existingFilePropList",currPropList.toString())
+	filePropWarningInfo.addProperty("skippedFileProperty","${entryKey} = ${entryValue}")
+	
+	BuildReportFactory.getBuildReport().addRecord(filePropWarningInfo)
 }
