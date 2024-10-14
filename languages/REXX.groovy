73d95d1c85dacc7d083201cadabd26405ea60cdf
Mathieu Dalbin <mathieu.dalbin@fr.ibm.com>
2024-10-14 15:31:44 +0200

    Checked properties with default values

--- languages/REXX.groovy
+++ languages/REXX.groovy
@@ -78,7 +78,6 @@ sortedList.each { buildFile ->
 		String needsLinking = props.getFileProperty('rexx_linkEdit', buildFile)
 		if (needsLinking && needsLinking.toBoolean()) {
 			rc = linkEdit.execute()
-			maxRC = props.getFileProperty('rexx_linkEditMaxRC', buildFile).toInteger()
 			if (props.getFileProperty('rexx_linkEditMaxRC', buildFile)) {
 				maxRC = props.getFileProperty('rexx_linkEditMaxRC', buildFile).toInteger()
 			} else {
