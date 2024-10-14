8735fef8f73790d5de3696cc9aaa992158ffdd2b
Mathieu Dalbin <mathieu.dalbin@fr.ibm.com>
2024-10-14 15:28:19 +0200

    Added checks for some properties
    Signed-off-by: Mathieu Dalbin <mathieu.dalbin@fr.ibm.com>

--- languages/REXX.groovy
+++ languages/REXX.groovy
@@ -57,8 +57,13 @@ sortedList.each { buildFile ->
 
 	// compile the cobol program
 	int rc = compile.execute()
-	int maxRC = props.getFileProperty('rexx_compileMaxRC', buildFile).toInteger()
-
+	int maxRC
+	if (props.getFileProperty('rexx_compileMaxRC', buildFile)) {
+		maxRC = props.getFileProperty('rexx_compileMaxRC', buildFile).toInteger()
+	} else {
+		maxRC = 4
+	}
+	
 	boolean bindFlag = true
 
 	if (rc > maxRC) {
@@ -71,9 +76,14 @@ sortedList.each { buildFile ->
 	else {
 		// if this program needs to be link edited . . .
 		String needsLinking = props.getFileProperty('rexx_linkEdit', buildFile)
-		if (needsLinking.toBoolean()) {
+		if (needsLinking && needsLinking.toBoolean()) {
 			rc = linkEdit.execute()
 			maxRC = props.getFileProperty('rexx_linkEditMaxRC', buildFile).toInteger()
+			if (props.getFileProperty('rexx_linkEditMaxRC', buildFile)) {
+				maxRC = props.getFileProperty('rexx_linkEditMaxRC', buildFile).toInteger()
+			} else {
+				maxRC = 4
+			}
 
 			if (rc > maxRC) {
 				String errorMsg = "*! The link edit return code ($rc) for $buildFile exceeded the maximum return code allowed ($maxRC)"
