180a08cc51e993f74a43c282ef9ad36f11b74847
AnjaliAbraham <47593670+AnjaliAbraham@users.noreply.github.com>
2025-08-29 13:01:35 +0530

    Update Easytrieve.groovy

--- languages/Easytrieve.groovy
+++ languages/Easytrieve.groovy
@@ -94,7 +94,7 @@ sortedList.each { buildFile ->
 				// only scan the load module if load module scanning turned on for file
 				String scanLoadModule = props.getFileProperty('easytrieve_scanLoadModule', buildFile)
 				if (scanLoadModule && scanLoadModule.toBoolean())
-					impactUtils.saveStaticLinkDependencies(buildFile, props.linkedit_loadPDS, logicalFile)
+					impactUtils.saveStaticLinkDependencies(buildFile, props.linkedit_loadPDS, member, logicalFile)
 			}
 		}
 			
